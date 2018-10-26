cd4bb41963c54345a6dca329f63b807b084123cd
Danilo Bias Lago <danilo@kanamobi.com.br>
2018-10-26 20:59:30 -0300

    Requests added;

---
+++ Animals/Animals/Requests/DogsRequests/DogsRequests.swift
@@ -0,0 +1,13 @@
+//
+//  DogsRequests.swift
+//  Animals
+//
+//  Created by Danilo Bias Lago on 26/10/2018.
+//  Copyright © 2018 Danilo Bias Lago. All rights reserved.
+//
+
+import UIKit
+
+class DogsRequests: NSObject {
+
+}
