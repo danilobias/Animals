2438b029224673c1177c93ce9bb18e939d40d84e
Danilo Bias Lago <danilo@kanamobi.com.br>
2018-10-26 20:51:39 -0300

    Constants added and Pod Kingfisher removed;

---
+++ Animals/Animals/Utils/Constants/GlobalConstants.swift
@@ -0,0 +1,47 @@
+//
+//  GlobalConstants.swift
+//  Animals
+//
+//  Created by Danilo Bias Lago on 26/10/2018.
+//  Copyright © 2018 Danilo Bias Lago. All rights reserved.
+//
+
+import UIKit
+
+//struct Constants {
+//    
+//    //MARK: URL's e métodos
+//    struct APIPreffix {
+//        static let urlCatPreffix: String = "https://api.thecatapi.com/v1/"
+//        static let urlDogPreffix: String = "https://api.thedogapi.com/v1/"
+//    }
+//    
+//    struct APIKeys {
+//        static let apiKeyCats: String = "0dde82cb-9a5b-4161-a264-32b6ea234f33"
+//        static let apiKeyDogs: String = "c93f79f2-2c0c-414b-adf9-c4fd2a7a687c"
+//    }
+//    
+//    struct URLPaths {
+//        static let commonPath = "/"
+//        static let concatKey = "&"
+//    }
+//    
+//    struct CommonMethods {
+//        static let search = "/search"
+//    }
+//    
+//    struct CatsMethods {
+//        
+//    }
+//    
+//    struct DogsMethods {
+//    }
+//    
+//    struct APIUrls {
+//        
+////        static let getAllCats = Constants.APIPreffix.urlPreffix + GenresMethods.getAllMethod + Constants.URLPaths.concatKey + Constants.SortMethods.orderByCreatedAtAsc + Constants.URLPaths.concatKey + Constants.APIKeys.apiKey
+////
+////        static let getAllDogs = Constants.APIPreffix.urlPreffix + GenresMethods.getMovieByGenreMethod + Constants.SortMethods.orderByCreatedAtAsc + Constants.URLPaths.concatKey + Constants.APIKeys.apiKey
+//        
+//    }
+//}
